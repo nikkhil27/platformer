5c5ba107b66f98e8db65469965d1bcac11d3ebfe
OnOnikhil <f20220041@pilani.bits-pilani.ac.in>
2025-12-23 13:47:57 +0530

    Changes made to add sprite

--- src/player.h
+++ src/player.h
@@ -4,11 +4,21 @@
 #include <stdbool.h>   
 #include "raylib.h"    
 
+typedef enum {
+    PLAYER_IDLE,
+    PLAYER_RUN,
+    PLAYER_JUMP,
+    PLAYER_FALL,
+} PlayerState;
+
 typedef struct Player {
     Rectangle rect;
     float velocityY;
     float velocityX;
     bool isOnGround;
+    PlayerState state;
+    bool facingRight;
+    int currentFrame;      // Which frame we're on
+    float frameTimer;      // Time accumulator for animation
 } Player;
-
 #endif
